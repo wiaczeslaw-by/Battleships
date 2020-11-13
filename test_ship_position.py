1ee8b5dd53ffc21df43ece3877e967c2fd599dde
WD <wojciech.dziadon@gmail.com>
2020-11-13 20:58:38 +0100

    poprawione test_ship_position

--- test_ship_position.py
+++ test_ship_position.py
@@ -5,19 +5,39 @@ def check_correct_positon (board,board_size, ship_size,user_row, user_col, ship_
     jesli oba warunki sa spełnione zwraca True
     NIESTETY NIE DZIAŁA JESLI STATEK PRZYLEGA DO KRAWĘDZI - musze jeszcze to poprawić"""
 
-    if ship_orientation = "horizontal" :
+    if ship_orientation == "horizontal" :
         test_count = 0
-        if user_col + ship_size <= board_size + :
+        if user_col + ship_size <= board_size + 1:
             test_count +=1
         
         space_test = 0
-        for row in range(user_row - 1, user_row + 1):
-            for column in range (user_col -1, user_col + 1):
-                if board[row][column] == "x"
-                space_test += 1
-        if space_test == (ship_size+2)*2+2:
-            test_count +1 = 0
+        for row in range(user_row - 1, user_row + 2):
+            for column in range (user_col -1, user_col + ship_size + 1):
+                if board[row][column] == "0":
+                    space_test += 1
+        if space_test == (ship_size+2)*3:
+            test_count +=1
         if test_count == 2:
             return True
         else :
             return False
+
+def init_board(board):    
+    for i in range(7):
+        board.append(["0"]*7)
+    
+    return board
+    
+def show_board(board):
+    for row in range(1,6):
+        print()
+        for column in range(1,6):
+            print(board[row][column], end="")
+    print()
+
+board = []
+init_board(board)
+show_board(board)
+print(check_correct_positon(board,5, 3,1,1,"horizontal"))
+
+
