65d415e33798c86c1acfbe48684a3a96f5a502e7
WD <wojciech.dziadon@gmail.com>
2020-11-20 22:14:44 +0100

    Wersja działająca, brak opcji gry z AI

--- battle_ver3.py
+++ battle_ver3.py
@@ -3,28 +3,50 @@ import random
 import sys
 
 def game_mode():
-    print("""Please chose game mode :
-                1 - Player vs Player
-                2 - Player vs Computer
-                3 - Player vs AI
-                """)
-    game_mode = input("Your choice is :")
-    return game_mode
+    correct_data = False
+    while correct_data != True:
+        print("""Please chose game mode :
+                    1 - Player vs Player
+                    2 - Player vs Computer
+                    3 - Player vs AI (XXX)
+                    """)        
+        game_mode = input("Your choice is :")
+        if game_mode in ["1","2","3"]:
+            correct_data = True
+            return game_mode
+        else:
+            print("Something went wrong, try again")
 
 def user_board_size():
-    print(""" Please chose board size :
-                1 - 5 x 5
-                2 - 10 x 10
-                3 - You can give a number of size
-                """)
-    user_choice = input("Your choice is :")
-    if user_choice == "1":
-        user_board_size = 7
-    elif user_choice == "2":
-        user_board_size = 12
-    elif user_board_size == "3":
-        user_board_size = int(input("How many field ? :"))
+    correct_data = False
+    while correct_data != True:
+        print(""" Please chose board size :
+                    1 - 5 x 5
+                    2 - 10 x 10
+                    3 - You can choose board size (from 6 to 9)
+                    """)
+        user_choice = input("Your choice is :")
+        if user_choice in ["1","2","3"]:
+            correct_data = True
+            if user_choice == "1":
+                user_board_size = 7
+            elif user_choice == "2":
+                user_board_size = 12
+            elif user_choice == "3":
+                user_board_size = set_user_board_size()+2
+        else :
+            print("Something went wrong, try again")
     return user_board_size
+
+def set_user_board_size():
+    correct_data = False
+    while correct_data != True:
+        board_size = int(input("What size do you choose? (from 6 to 9) :"))
+        if board_size in [6,7,8,9]:
+            correct_data = True
+            return board_size
+        else :
+            print("Something went wrong, try again")
 
 def check_fit (board_size, ship_size, user_row, user_column, ship_orientation):
         if ship_orientation == "horizontal" :
@@ -97,32 +119,68 @@ def get_ship_coridnates():
             Column (from 1 to 5))
             Enter the row first :
             """)
-    user_row_letter = input("Enter the row first :")
-    user_row = int(ord(user_row_letter.lower())- 96)
-    
-    user_column = int(input("Enter the column now :"))
-    return user_row, user_column
+    user_row, user_column = get_coordinates(board_size)
+    return user_row,user_column
 
-def get_shot_coridnates():
-    print("""Chose shot position. (first field).
-            Row - (from A to E)
-            Column (from 1 to 5))
-            Enter the row first :
-            """)
-    user_row_letter = input("Enter the row first :")
-    user_row = int(ord(user_row_letter.lower())- 96)
-    
-    user_column = int(input("Enter the column now :"))
+def get_shot_coridnates(board_displayed):
+    correct_data = False
+    while correct_data != True:
+        print("""Chose shot position. (first field).
+                Row - (from A to E)
+                Column (from 1 to 5))
+                Enter the row first :
+                """)
+        user_row, user_column = get_coordinates(board_size)
+        if board_displayed[user_row][user_column] == "0":
+            correct_data = True
+        else:
+            print("You have already given this coordinate. Try again")
+    return user_row,user_column
+
+def get_coordinates(board_size):
+    good_data = False
+    while good_data != True:
+        user_row_letter = input("Enter the row first :")
+        if board_size == 7:
+            if user_row_letter.lower() not in letter_5x5:
+                print("Not an appropriate choice")
+            else :
+                user_row = int(ord(user_row_letter.lower())- 96)
+                good_data = True
+        elif board_size == 12:
+            if user_row_letter.lower() not in letter_10x10:
+                print("Not an appropriate choice")
+            else :
+                user_row = int(ord(user_row_letter.lower())- 96)
+                good_data = True
+    good_data = False
+    while good_data != True:
+        try:
+            user_column = int(input("Enter the column now :"))
+            if board_size == 7:
+                if user_column > 5:
+                    print("Not an appropriate choice")
+                else :
+                    good_data = True
+            if board_size == 12:
+                if user_column > 10:
+                    print("Not an appropriate choice")
+                else :
+                    good_data = True
+        except ValueError:
+            print("Something went wrong.Please try again")
     return user_row, user_column
 
 def random_shot_coordinates(board_size,board_displayed):
-    random_row = random.randint(1,board_size - 2)
-    random_column = random.randint(1,board_size - 2)
-    if board_displayed[random_row][random_column] == "0":
-        return random_row, random_column
-    else :    
-        random_shot_coordinates(board_size,board_displayed)
-    
+    corect_data = False
+    while corect_data != True:
+        random_row = random.randint(1,board_size - 2)
+        random_column = random.randint(1,board_size - 2)
+        if board_displayed[random_row][random_column] == "0":
+            corect_data = True
+        else :    
+            pass
+    return random_row, random_column
     
 def take_value_field(board,row,column):
     return board[row][column]
@@ -143,9 +201,8 @@ def set_ships(ship_list,board):
         completed_ship = False
         os.system("cls || clear")
         show_board(board, board_size)
-        while completed_ship == False:
-            os.system("cls || clear")
-            show_board(board, board_size)
+        while completed_ship == False:            
+            #show_board(board, board_size)
             print (f'Set position for the ship of size: {ship_list[ship]} \n')
             ship_size = ship_list[ship]        
             ship_orientation = get_ship_orientation()
@@ -153,8 +210,9 @@ def set_ships(ship_list,board):
             if check_correct_positon(board, ship_size, user_row, user_column, ship_orientation) is True and check_fit(board_size,ship_size,user_row,user_column,ship_orientation):
                 mark_ships(board, ship_size, ship_orientation, user_row, user_column)
                 completed_ship = True
+                os.system("cls || clear")
             else:
-                print("You cant put ship here. Try again")
+                print("You cant put ship here. Try again")                
         show_board(board, board_size)  
 
 def random_set_ships(ship_list,board):
@@ -198,14 +256,11 @@ def random_ship_cordinates(board_size):
         ship_orientation = "horizontal"
     else :
         ship_orientation = "vertical"
-    #print(ship_orientation)
     random_row = random.randint(1,board_size - 2)
     random_column = random.randint(1,board_size - 2)
-    #print(random_row,",",random_column)
     return ship_orientation, random_row, random_column
 
 def give_a_shot(player,sequence):
-    #win = False
     if player == "player_2":
         board = board_1
         board_displayed = board_1_displayed
@@ -216,26 +271,29 @@ def give_a_shot(player,sequence):
     while next_shot == True:
         print(f"Now {player}")
         if game_mode == "1":
-            user_shot_row, user_shot_column = get_shot_coridnates()
+            user_shot_row, user_shot_column = get_shot_coridnates(board_displayed)
         if game_mode == "2" and sequence % 2 == 0:
-            user_shot_row, user_shot_column = get_shot_coridnates()
+            user_shot_row, user_shot_column = get_shot_coridnates(board_displayed)
         else :            
             user_shot_row, user_shot_column = random_shot_coordinates(board_size,board_displayed)
         user_shot_value = take_value_field(board,user_shot_row,user_shot_column)
         print(f'You chose {user_shot_row} {user_shot_column}')
         if user_shot_value == "0":
             board_displayed[user_shot_row][user_shot_column] = "M"
-            print("You've missed!")
+            os.system("cls || clear")
+            print("You've missed!")            
             next_shot = False
         elif user_shot_value == "X":
             mark_board(board_displayed,user_shot_row,user_shot_column,"H")
             ship_elements.clear()
             check_adjacent(board,user_shot_row,user_shot_column, "X")
             if is_sunk(board_displayed) is True:
-                print("You've sunk a ship!")
+                os.system("cls || clear")
+                print("You've sunk a ship!\n")
                 mark_ship_sunk(board_displayed)
             else:
-                print("You've hit a ship!")
+                os.system("cls || clear")
+                print("You've hit a ship!\n")
         win = has_won(board_displayed)
         if win == True:
             end_game(player)
@@ -243,15 +301,21 @@ def give_a_shot(player,sequence):
         two_boards(board_1_displayed, board_2_displayed)
 
 def has_won(board):
-    count = 0    
+    count_s_element = 0    
     for row in range(len(board)):
         for column in range(len(board)):
             if board[row][column] == "S":
-                count += 1
-    if count == 5:        
-        return True
-    else:   
-        return False
+                count_s_element += 1
+    if len(board) == 7:
+        if count == 5:        
+            return True
+        else:   
+            return False
+    if len(board) == 12:
+        if count_s_element == 20:
+            return True
+        else:   
+            return False
 
 def check_adjacent(board, row, column, sign):
     if board[row+1][column] == sign and check_ship_elements(row+1,column) == False:
@@ -309,17 +373,42 @@ def mark_ship_sunk(board_displayed):
 def end_game(text):
     print(text, "wins !!!")
 
-def fill_boards(game_mode):
+def choose_random_filling():
+    correct_data = False
+    if correct_data != True:
+        print("""Do you want to randomly fill the board.
+                Y - Yes
+                N - No""")
+        random_filling = input('Choose "y" or "n": ').lower()
+        if random_filling == "y":
+            correct_data = True
+            return True
+        elif random_filling == "n":
+            correct_data = True
+            return False
+        else:
+            print("Something went wrong.Please try again")
+
+def fill_empty_boards(game_mode):
     if game_mode == "1":
-        set_ships(ship_list, board_1)
+        if choose_random_filling() is True:
+            random_set_ships(ship_list, board_1)
+        else :
+            set_ships(ship_list, board_1)
         print("You have finished positioning the ships")
         os.system("cls || clear")
         input("Press ENTER to allow the next player to position the ships")
-        set_ships(ship_list, board_2)
+        if choose_random_filling() is True:
+            random_set_ships(ship_list, board_2)
+        else :
+            set_ships(ship_list, board_2)
         print("You have finished positioning the ships")
         os.system("cls || clear")
     else :
-        set_ships(ship_list, board_1)
+        if choose_random_filling() is True:
+            random_set_ships(ship_list, board_1)
+        else :
+            set_ships(ship_list, board_1)
         print("You have finished positioning the ships")
         os.system("cls || clear")
         input("Press ENTER to allow the computer to position the ships")
@@ -338,6 +427,8 @@ ship_list = []
 ship_elements = []
 ship_5_5 = [2,1,1,1]
 ship_10_10 =[4,3,3,2,2,2,1,1,1,1]
+letter_5x5 = ("a","b","c","d","e")
+letter_10x10 = ("a","b","c","d","e","f","g","h","i","j")
 
 game_mode = game_mode()
 board_size = user_board_size()
@@ -353,5 +444,5 @@ elif board_size == 12:
 else:
     ship_list = ship_10_10
 
-fill_boards(game_mode)
+fill_empty_boards(game_mode)
 play_game()
